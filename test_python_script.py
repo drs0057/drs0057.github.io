74cb0cf26e7daae172f289c0705b5e088db4840c
Derek Swanson <drs0057@auburn.edu>
2024-01-31 13:04:22 -0600

    Fix python linting

--- test_python_script.py
+++ test_python_script.py
@@ -1,4 +1,12 @@
+"""
+This module serves to test a basic pylint CI action.
+"""
+
+
 def main():
+    """Do I really need a docstring here?"""
     return "hello python"
 
-main()
+
+if __name__ == "__main__":
+    main()
